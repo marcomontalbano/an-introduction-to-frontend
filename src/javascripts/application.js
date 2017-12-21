86e325c5427658b1b7412b5ac50b537a21dd0e48
Matteo Colombo Speroni <matteo.colombosperoni@gmail.com>
2017-12-21 23:08:29 +0100

    changes to integrate with real back end application created into be course

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -2,7 +2,7 @@
 // manage cookie policy
 $('.cookie-policy .cookie-policy--close').on('click', function() {
     $(this).closest('.cookie-policy').fadeOut();
-})
+});
 
 // Ajax Like
 $(document).on('click', '.article .article--like', function()
@@ -12,11 +12,12 @@ $(document).on('click', '.article .article--like', function()
     var articleId = $article.data('articleId');
 
     $.ajax({
-        url: 'like.json',
-        method: 'GET', // 'PUT',
-        data: {
-            id: articleId,
-        },
+        url: 'http://localhost:8080/articles/like',
+        method: 'PATCH', // 'PUT',
+        contentType: "application/json",
+        data: JSON.stringify({
+            id: articleId
+        }),
         success: function (data) {
             console.log('data', data);
             $button.toggleClass('btn-success', data.like);
@@ -26,28 +27,35 @@ $(document).on('click', '.article .article--like', function()
 
 // Load Articles
 $(function() {
-    var $articles = $('.articles');
-    var $articlePlaceholder = $articles.find('.article.hide');
 
     $.ajax({
-        url: 'articles.json',
+        url: 'http://localhost:8080/articles',
         method: 'GET',
-        success: function (articles) {
-            $articles.empty();
-            for (var i = 0; i < articles.length; i++) {
-                var $article = $articlePlaceholder.clone();
-                var article = articles[i];
+        dataType: 'jsonp',
+        jsonpCallback: 'loadArticles'
 
-                $article.removeClass('hide');
-                $article.data('articleId', article.id);
-                $article.find('.article--title').text(article.title);
-                $article.find('.article--picture').attr('src', article.picture);
-                $article.find('.article--plot').text(article.plot);
-                $article.find('.article--tag').text(article.tag);
-                $article.find('.article--like').toggleClass('btn-success', article.like);
-                $article.find();
-                $articles.append($article);
-            }
-        }
     });
+
 });
+
+function loadArticles (articles) {
+    var $articles = $('.articles');
+    var $articlePlaceholder = $articles.find('.article.hide');
+    $articles.empty();
+    for (var i = 0; i < articles.length; i++) {
+        var $article = $articlePlaceholder.clone();
+        var article = articles[i];
+
+        $article.removeClass('hide');
+        $article.data('articleId', article.id);
+        $article.find('.article--title').text(article.title);
+        $article.find('.article--picture').attr('src', article.picture);
+        $article.find('.article--plot').text(article.plot);
+        $article.find('.article--tag').text(article.tag);
+        $article.find('.article--like').toggleClass('btn-success', article.like);
+        $article.find();
+        $articles.append($article);
+    }
+}
+
+
