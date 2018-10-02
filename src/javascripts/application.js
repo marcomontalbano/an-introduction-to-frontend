2288f417f7d9281c9ef84f3de56e4a045493d01b
Marco Montalbano <marcomontalbano.work@gmail.com>
2018-10-02 23:42:00 +0200

    Replaced 'application/x-www-form-urlencoded' with 'application/json'.

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -16,9 +16,10 @@ $(document).on('click', '.article .article--like', function(e)
     $.ajax({
         url: 'https://my-json-server.typicode.com/marcomontalbano/an-introduction-to-frontend-for-beginners/articles/' + articleId,
         method: 'PATCH',
-        data: {
+        contentType: 'application/json',
+        data: JSON.stringify({
             like: !$button.hasClass('btn-success')
-        },
+        }),
         success: function (article) {
             $button.toggleClass('btn-success', article.like);
         }
@@ -35,6 +36,7 @@ $(function() {
     $.ajax({
         url: 'https://my-json-server.typicode.com/marcomontalbano/an-introduction-to-frontend-for-beginners/articles',
         method: 'GET',
+        contentType: 'application/json',
         success: function (articles) {
             $.each(articles, function (index, article) {
                 html = template(article);
